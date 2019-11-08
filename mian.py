4319cd2124c0e66c370f7449785f376c0e6c813f
JaviBite <javiergimenezgarces@gmail.com>
2019-11-08 23:31:07 +0100

    req

--- mian.py
+++ mian.py
@@ -7,9 +7,17 @@ import speech_recognition as sr
 # Import the required module for text
 # to speech conversion
 from gtts import gTTS
+import pyttsx3
+import os
 
 # obtain audio from the microphone
 r = sr.Recognizer()
+engine = pyttsx3.init()
+engine.setProperty('volume', 10.0)
+engine.setProperty('rate', 115)
+
+engine.say("Hola soy tu asistente de voz")
+engine.runAndWait()
 with sr.Microphone() as source:
     while True:
         print("Say something!")
@@ -18,7 +26,10 @@ with sr.Microphone() as source:
 
         WIT_AI_KEY = "Y3FGBAGFPLAM5FU2LIO6WM6EBAZU3AHN"  # Wit.ai keys are 32-character uppercase alphanumeric strings
         try:
-            print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+            voice_text = r.recognize_wit(audio, key=WIT_AI_KEY)
+            print("Wit.ai thinks you said " + voice_text)
+            engine.say(voice_text)
+            engine.runAndWait()
         except sr.UnknownValueError:
             print("Wit.ai could not understand audio")
         except sr.RequestError as e:
