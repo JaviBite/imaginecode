63a9cdd6fe7d5b76de11358ad33dbbf38756d55f
JaviBite <javiergimenezgarces@gmail.com>
2019-11-09 10:51:52 +0100

    Holi

--- mian.py
+++ mian.py
@@ -3,22 +3,40 @@
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
 import speech_recognition as sr
-
+from enum import Enum
 # Import the required module for text
 # to speech conversion
 from word2number import w2n
 import pyttsx3
-
-class cont:
-    name = ""
-    items = ""
-    n = 0
+import os
 
 # obtain audio from the microphone
 r = sr.Recognizer()
 engine = pyttsx3.init()
 engine.setProperty('volume', 10.0)
 engine.setProperty('rate', 115)
+
+class Boxes(Enum):
+    Pink = 1 #Rosa
+    Green = 2 #Verde
+    Yellow = 3 #Amarillo
+    Blue = 4 #Azul
+    Red = 5 #Rojo
+    White = 6 #Blanco
+    Black = 7 #Negro
+    Orange = 8 #Naranja
+    Purple = 9 #Morado
+    Brown = 10 #Marron
+    Grey = 11 #Gris
+    Silver = 12 #Plateado
+    Gold = 13 #Dorado
+    Beige = 14 #Beige
+    Lilac = 15 #Lila
+    Light_blue = 16 #Azul clarito
+    Dark_blue = 17 #Azul oscuro
+    Violeta = 18 #Violet
+    Turqoise = 19 #Turquesa
+    Magenta = 20 #Magenta
 
 engine.say("Hola soy tu asistente de voz")
 engine.runAndWait()
@@ -87,20 +105,33 @@ with sr.Microphone() as source:
         WIT_AI_KEY = "Y3FGBAGFPLAM5FU2LIO6WM6EBAZU3AHN"  # Wit.ai keys are 32-character uppercase alphanumeric strings
         try:
             voice_text = r.recognize_wit(audio, key=WIT_AI_KEY)
-            print("Wit.ai thinks you said: " + voice_text)
+
             number = w2n.word_to_num(voice_text)
             print("Nums: " + str(number) )
+
+            print("Creo que has dicho: " + voice_text)
             engine.say(voice_text)
             engine.runAndWait()
 
 
-
-
-
         except ValueError:
             engine.say("No se ha reconocido nigun numero")
+
         except sr.UnknownValueError:
-            print("Wit.ai could not understand audio")
+            engine.say("Lo siento, no he entendido lo que has dicho")
+            engine.runAndWait()
         except sr.RequestError as e:
             print("Could not request results from Wit.ai service; {0}".format(e))
 
+
+# Detecta el color de la caja
+def contains_color (voice_text):
+    for color in Boxes:
+        if color in voice_text:
+            return color
+
+# El color de la caja de destino será el detectado
+def Search(voice_text):
+    if "destiny" in voice_text:
+        for box in cont_d:
+            color = contains_color(voice_text)
